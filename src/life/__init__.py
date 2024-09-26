1dceefe36f7b186486288c42008e0c9532d7a45d
ahmadhanif946 <ahmadhanif946@gmail.com>
2024-09-26 22:10:26 +0200

    Implementing NLeSC python template for structuring the life program

---
+++ src/life/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about life."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Ahmed Hanif"
+__email__ = "ahmadhanif946@gmail.com"
+__version__ = "0.1.0"
